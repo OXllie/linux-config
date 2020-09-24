edf033bdaa55ef77a79632f8e679cb46c7dc3314
Oliver <40476472+OXllie@users.noreply.github.com>
2020-09-24 15:57:16 +0100

    added setup

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+EXCLUDE=('.git' '.gitignore' 'setup.sh' '.setup.sh.swp')
+LINKS=(`ls -A`)
+
+for i in ${!LINKS[@]}; do
+	[[ ${EXCLUDE[@]} =~ ${LINKS[$i]} ]] && unset LINKS[$i]
+done
+
+for i in ${LINKS[@]}; do
+	[[ -h ~/$i ]] && continue
+	[[ -f ~/$i ]] && echo "File or directory already exists: $i" && continue
+	echo "Creating symlink for $i"
+	$(ln -sr $i ~/)
+done
+
+[[ -z $(which xclip) ]] && echo "Install xclip for tmux"
